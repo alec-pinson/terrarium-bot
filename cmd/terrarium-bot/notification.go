275410d8170ca078fd9a0f910a5d5a3016b77d8a
Alec Pinson <alec.pinson01@currys.co.uk>
2022-10-31 20:54:32 +0000

    add pushover notifications

--- cmd/terrarium-bot/notification.go
+++ cmd/terrarium-bot/notification.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"time"
+
+	"github.com/gregdel/pushover"
 )
 
 var lastNotificationTime time.Time
@@ -11,7 +13,27 @@ func SendNotification(alertMessage string) {
 	if lastNotificationTime.Add(c.Alerts.AntiSpam.Sleep).Before(time.Now()) {
 		log.Println("SendNotification(): Sent alert: '" + alertMessage + "'")
 		lastNotificationTime = time.Now()
+		// PushoverNotification(alertMessage)
 	} else {
 		log.Println("SendNotification(): Alert not sent: '" + alertMessage + "' (anti-spam)")
+	}
+}
+
+func PushoverNotification(alertMessage string) {
+	app := pushover.New(c.Alerts.Pushover.APIToken)
+	recipient := pushover.NewRecipient(c.Alerts.Pushover.UserToken)
+
+	message := &pushover.Message{
+		Message:    alertMessage,
+		DeviceName: c.Alerts.Pushover.Device,
+	}
+
+	response, err := app.SendMessage(message, recipient)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if c.Debug {
+		log.Println(response)
 	}
 }

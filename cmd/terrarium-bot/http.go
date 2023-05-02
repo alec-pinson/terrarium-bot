e2331999fabef59b71138953726ca9628d56b1d5
Alec <30310787+alec-pinson@users.noreply.github.com>
2023-05-02 13:59:00 +0100

    Only attempt json decode on responses if needed (#10)
    * Only attempt json decode on responses if needed
    
    * fix increasing number of keep alive packets due to connections not closing
    
    * use http client pools
    
    * remove failing test for now

--- cmd/terrarium-bot/http.go
+++ cmd/terrarium-bot/http.go
@@ -1,29 +1,77 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httptrace"
 	"net/http/httputil"
+	"net/url"
+	"strings"
 	"time"
 )
 
-func SendRequest(url string, insecure bool, retries int) (map[string]interface{}, int, error) {
+var httpClientPool []*HttpClientPool
+
+type HttpClientPool struct {
+	Hostname string
+	Client   http.Client
+}
+
+func getClient(address string, insecure bool) *http.Client {
+	url, err := url.Parse(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	hostname := strings.TrimPrefix(url.Hostname(), "www.")
+
+	// if there's an existing client, return it
+	for _, pool := range httpClientPool {
+		if pool.Hostname == hostname {
+			return &pool.Client
+		}
+	}
+	// otherwise create a new client and return that
+	Debug("Creating http pool for %s", hostname)
+	client := http.Client{Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+	}}
+	httpClientPool = append(httpClientPool, &HttpClientPool{
+		Hostname: hostname,
+		Client:   client,
+	})
+	return &client
+}
+
+func SendRequest(url string, insecure bool, retries int, decodeJson bool) (map[string]interface{}, int, error) {
 	result := map[string]interface{}{}
+	var req *http.Request
 	var resp *http.Response
 	var err error
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+	if isTesting {
+		// client trace to log whether the request's underlying tcp connection was re-used
+		traceCtx := httptrace.WithClientTrace(context.Background(), &httptrace.ClientTrace{
+			GotConn: func(info httptrace.GotConnInfo) { log.Printf("conn was reused: %t", info.Reused) },
+		})
+
+		req, err = http.NewRequestWithContext(traceCtx, http.MethodGet, url, nil)
+	} else {
+		req, err = http.NewRequest(http.MethodGet, url, nil)
 	}
-	client := &http.Client{Transport: tr}
+
+	// set http insecure mode true/false
+	client := getClient(url, insecure)
 
 	// retry x times if an error occurs, sleep 1 second each time
 	for i := 0; i < retries; i++ {
 		Debug("Request attempt %v/%v", i+1, retries)
-		resp, err = client.Get(url)
+		resp, err = client.Do(req)
 		if err == nil && resp.StatusCode == 200 {
 			break
 		}
@@ -46,11 +94,16 @@ func SendRequest(url string, insecure bool, retries int) (map[string]interface{}
 		fmt.Printf("Response\n: %v\n\n", string(responseDump))
 	}
 
-	// attempt decode and return response
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&result); err != nil {
-		log.Printf("Request to %v: could not decode response: %v", url, err)
-		return nil, resp.StatusCode, err
+	if decodeJson {
+		// attempt decode and return response
+		decoder := json.NewDecoder(resp.Body)
+		if err := decoder.Decode(&result); err != nil {
+			log.Printf("Request to %v: could not decode response: %v", url, err)
+			return nil, resp.StatusCode, err
+		}
+	} else {
+		// need to read the resp body in order for the connection to be reused https://golang.cafe/blog/how-to-reuse-http-connections-in-go.html
+		io.Copy(ioutil.Discard, resp.Body)
 	}
 	return result, resp.StatusCode, nil
 }
--- cmd/terrarium-bot/http_test.go
+++ cmd/terrarium-bot/http_test.go
@@ -13,26 +13,26 @@ import (
 )
 
 func TestSendRequest(t *testing.T) {
+	// Mock http request response
+	mockResponse := `{"key1": "value1", "key2": 2, "key3": ["element1", "element2"]}`
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, mockResponse)
+	})
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
 	t.Run("Valid URL", func(t *testing.T) {
 		expected := map[string]interface{}{
 			"key1": "value1",
 			"key2": float64(2),
 			"key3": []interface{}{"element1", "element2"},
 		}
-		mockResponse := `{"key1": "value1", "key2": 2, "key3": ["element1", "element2"]}`
-
-		// Mock http request response
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprint(w, mockResponse)
-		})
-		server := httptest.NewServer(mux)
-		defer server.Close()
 
 		// Send request to mocked server URL
-		res, respCode, err := SendRequest(server.URL, false, 1)
+		res, respCode, err := SendRequest(server.URL, false, 1, true)
 		if err != nil {
 			t.Fatalf("Unexpected error: %v", err)
 		}
@@ -47,7 +47,7 @@ func TestSendRequest(t *testing.T) {
 	})
 
 	t.Run("Invalid URL", func(t *testing.T) {
-		_, respCode, err := SendRequest("invalid_url", false, 1)
+		_, respCode, err := SendRequest("invalid_url", false, 1, false)
 		if err == nil {
 			t.Error("Expected error, but got nil")
 		}
@@ -61,7 +61,7 @@ func TestSendRequest(t *testing.T) {
 		var buf bytes.Buffer
 		log.SetOutput(&buf)
 
-		_, respCode, err := SendRequest("invalid_url", false, 3)
+		_, respCode, err := SendRequest("invalid_url", false, 3, false)
 		if err == nil {
 			t.Error("Expected error, but got nil")
 		}

71a40107905c838b1bcb16d7a9cbe067c4c8720c
Alec Pinson <30310787+alec-pinson@users.noreply.github.com>
2023-05-05 14:33:56 +0100

    forgot this

--- cmd/terrarium-bot/metrics.go
+++ cmd/terrarium-bot/metrics.go
@@ -13,7 +13,7 @@ type Metrics struct{}
 func (metrics Metrics) Start() {
 	log.Println("Starting Metrics server...")
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8081", nil)
+	go http.ListenAndServe(":8081", nil)
 	time.Sleep(1 * time.Second) // give some time for metrics server to start before moving on
 	log.Println("Metrics Server started...")
 }
